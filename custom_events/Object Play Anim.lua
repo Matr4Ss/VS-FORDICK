ec70f02f6304cec4f6c55e8e199000c9448da165
Matr4Ss <121343722+Matr4Ss@users.noreply.github.com>
2022-12-27 20:25:03 +0300

    Delete custom_events directory

--- custom_events/Object Play Anim.lua
+++
@@ -1,9 +0,0 @@
-animToPlay = ''
-obj = ''
-function onEvent(name,value1,value2)
-	if name == 'Object Play Anim' then
-		animToPlay = value1
-		obj = value2
-		playAnim(obj, animToPlay)
-	end
-end
--- custom_events/Set Property.lua
+++
@@ -1,5 +0,0 @@
-function onEvent(name, value1, value2)
-	if name == 'Set Property' then
-		setProperty(value1, value2)
-	end
-end
